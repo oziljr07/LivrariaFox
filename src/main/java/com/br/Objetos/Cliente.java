f91fc25f81247d7c4ae81d1f009f88d24b2755aa
oziel.240016 <oziel.240016@L-1-17-06--07.satc.edu.br>
2018-08-07 16:08:23 -0300

    Classe Client

---
+++ src/main/java/com/br/Objetos/Cliente.java
@@ -0,0 +1,89 @@
+
+package com.br.Objetos;
+
+
+public class Cliente {
+
+   private long id;
+   private String nome;
+   private String rg;
+   private String cpf;
+   private String usuario;
+   private String senha;
+   private String email;
+   private int telefone;
+   
+   
+   
+   
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+   
+   
+           
+
+    
+}
